c73f221c35fc0c1abb7f473995cced288cc5062a
srksifat-dev <srksifat.dev@gmail.com>
2023-04-14 10:22:41 +0600

    Onboarding Screen first page designing  done

--- lib/feature/onboarding/screen/onboarding_screen.dart
+++ lib/feature/onboarding/screen/onboarding_screen.dart
@@ -13,12 +13,11 @@ class OnboardingScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                height: MediaQuery.of(context).size.height * 0.5,
+                height: MediaQuery.of(context).size.height * 0.6,
                 width: MediaQuery.of(context).size.width,
-                color: Colors.amber,
                 child: Center(
-                    child:
-                        Text("Ekhane ekti Image bosbe")), //TODO: Place an Image
+                    child: Image.asset(
+                        "assets/images/onboarding-01.png")), //TODO: Place an Image
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
@@ -26,6 +25,41 @@ class OnboardingScreen extends StatelessWidget {
                   "NO MORE\nBORING THINGS",
                   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                 ),
+              ),
+              Text(
+                "Picking up accessories from\npopular European brands.",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Skip",
+                        style: TextStyle(color: Colors.grey, fontSize: 20),
+                      )),
+                  TextButton(
+                    style: ButtonStyle(
+                        padding: MaterialStatePropertyAll(EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 16,
+                        )),
+                        shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16))),
+                        backgroundColor:
+                            MaterialStatePropertyAll(Colors.black)),
+                    onPressed: () {},
+                    child: Text(
+                      "Next",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  )
+                ],
               )
             ],
           ),
