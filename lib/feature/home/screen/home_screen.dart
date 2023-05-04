7bd33d7fbe8f0490638e3a5fcf634402ff16f6a5
srksifat-dev <srksifat.dev@gmail.com>
2023-05-04 10:12:37 +0600

    Home screen has been started

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-const HomeScreen({ Key? key }) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int tabIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: tabIndex,
+        onTap: (value) {
+          setState(() {
+            tabIndex = value;
+          });
+        },
+        selectedItemColor: Colors.amber,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+              backgroundColor: Color(0xff23272c)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Favorite",
+              backgroundColor: Color(0xff23272c)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag),
+              label: "Cart",
+              backgroundColor: Color(0xff23272c)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+              backgroundColor: Color(0xff23272c)),
+        ],
+      ),
+    );
   }
 }
