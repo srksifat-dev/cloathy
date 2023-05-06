69267287cf1d4066de483f3226bad733c3269dc1
srksifat-dev <srksifat.dev@gmail.com>
2023-05-06 08:51:54 +0600

    Home screen designing has been started

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -8,11 +8,69 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  FocusNode searchFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Home"),
+    return GestureDetector(
+      onTap: () => searchFocusNode.unfocus(),
+      child: Scaffold(
+        backgroundColor: Color(0xfff3f3f3),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      focusNode: searchFocusNode,
+                      cursorColor: Colors.black,
+                      cursorHeight: 30,
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        suffixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                      ),
+                    )),
+                    IconButton(
+                        onPressed: () {
+                          showBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Container(
+                                  height: 100,
+                                  color: Colors.amber,
+                                );
+                              });
+                        },
+                        icon: Icon(Icons.filter_alt))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
