1181d0cb07f6c493b4e25b758db800d59d467b0a
srksifat-dev <srksifat.dev@gmail.com>
2023-05-20 12:43:28 +0600

    New Item section design done

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -15,129 +15,196 @@ class _HomeScreenState extends State<HomeScreen> {
   FocusNode searchFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xfff3f3f3),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                      child: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(CupertinoPageRoute(
-                          builder: (context) => SearchScreen()));
-                    },
-                    // Search and filtering section
-                    child: Container(
-                      height: 60,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2),
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 16),
-                            child: Text(
-                              "Search",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 18),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 16),
-                            child: Icon(
-                              Icons.search,
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  )),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-
-              // Category section
-              SizedBox(
-                height: 100,
-                width: MediaQuery.of(context).size.width - 32,
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return categoryCard(
-                        label: categories[index].label,
-                        icon: categories[index].icon);
-                  },
-                  separatorBuilder: (_, __) {
-                    return SizedBox(
-                      width: 16,
-                    );
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-
-              // Offer section
-              Card(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 80,
-                        width: 80,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(CupertinoPageRoute(
+                            builder: (context) => SearchScreen()));
+                      },
+                      // Search and filtering section
+                      child: Container(
+                        height: 60,
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            color: Color(0xfff3f3f3)),
-                        child: Center(
-                          child: Icon(
-                            Icons.percent,
-                            size: 40,
-                          ),
+                          border: Border.all(color: Colors.black, width: 2),
+                          borderRadius: BorderRadius.circular(16),
                         ),
-                      ),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              "50% OFF",
-                              style: TextStyle(
-                                  fontSize: 35, fontWeight: FontWeight.bold),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 16),
+                              child: Text(
+                                "Search",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 18),
+                              ),
                             ),
-                            Text(
-                              "on all woman's shoes",
-                              style: TextStyle(fontSize: 20),
-                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 16),
+                              child: Icon(
+                                Icons.search,
+                                color: Colors.grey,
+                              ),
+                            )
                           ],
                         ),
                       ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.grey,
-                      )
-                    ],
+                    )),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+
+                // Category section
+                SizedBox(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width - 32,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      return categoryCard(
+                          label: categories[index].label,
+                          icon: categories[index].icon);
+                    },
+                    separatorBuilder: (_, __) {
+                      return SizedBox(
+                        width: 16,
+                      );
+                    },
                   ),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 16,
+                ),
+
+                // Offer section
+                Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: Color(0xfff3f3f3)),
+                          child: Center(
+                            child: Icon(
+                              Icons.percent,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "50% OFF",
+                                style: TextStyle(
+                                    fontSize: 35, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "on all woman's shoes",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.grey,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+                // New Item Section
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "New Item",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 30,
+                  width: MediaQuery.of(context).size.width * 100,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return SizedBox(
+                        width: 130,
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(16),
+                                  child: Image.asset(
+                                    "assets/images/men.jpg",
+                                    height: 200,
+                                  ),
+                                ),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.favorite_border))
+                              ],
+                            ),
+                            Text(
+                              "Product Name",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.grey),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "৳ 300",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                IconButton(
+                                    onPressed: () {}, icon: Icon(Icons.add))
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (_, __) => SizedBox(
+                      width: 16,
+                    ),
+                    itemCount: 10,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
