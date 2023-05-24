a18dfe7b925d0c34411366977c946726353116b9
srksifat-dev <srksifat.dev@gmail.com>
2023-05-24 12:27:40 +0600

    Favorite screen done

--- lib/feature/favorite/screen/favorite_screen.dart
+++ lib/feature/favorite/screen/favorite_screen.dart
@@ -1,12 +1,73 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class FavoriteScreen extends StatelessWidget {
-const FavoriteScreen({ Key? key }) : super(key: key);
+  const FavoriteScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Favorite"),),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Text(
+                  "Your Favorites",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 8,
+                      childAspectRatio: 0.8),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.asset(
+                                "assets/images/men.jpg",
+                                height: 160,
+                              ),
+                            ),
+                            IconButton(
+                                onPressed: () {}, icon: Icon(Icons.favorite))
+                          ],
+                        ),
+                        Text(
+                          "Product Name",
+                          style: TextStyle(fontSize: 20, color: Colors.grey),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "৳ 300",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            IconButton(onPressed: () {}, icon: Icon(Icons.add))
+                          ],
+                        )
+                      ],
+                    );
+                  },
+                  itemCount: 10,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
