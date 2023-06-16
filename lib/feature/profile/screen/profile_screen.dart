ec01931eb8a42a13712850e4462f65e63aee1485
srksifat-dev <srksifat.dev@gmail.com>
2023-06-16 11:02:15 +0600

    Updating address functionality implemented

--- lib/feature/profile/screen/profile_screen.dart
+++ lib/feature/profile/screen/profile_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  TextEditingController districtController = TextEditingController();
+  TextEditingController areaController = TextEditingController();
+  TextEditingController detailsController = TextEditingController();
+
+  FocusNode districtFocusNode = FocusNode();
+  FocusNode areaFocusNode = FocusNode();
+  FocusNode detailsFocusNode = FocusNode();
+
+  late String district;
+  late String area;
+  late String details;
+
+  @override
+  void initState() {
+    district = "Your District";
+    area = "Your Area";
+    details = "Your Details Address";
+    districtController.text = district;
+    areaController.text = area;
+    detailsController.text = details;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,30 +58,78 @@ class ProfileScreen extends StatelessWidget {
               const SizedBox(
                 height: 16,
               ),
-              Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)),
-                child: const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Addresses",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+              GestureDetector(
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: Text("Address"),
+                            content: SizedBox(
+                              height: 200,
+                              child: Column(
+                                children: [
+                                  TextField(
+                                      controller: districtController,
+                                      focusNode: districtFocusNode,
+                                      decoration: InputDecoration(
+                                          hintText: "District")),
+                                  TextField(
+                                      controller: areaController,
+                                      focusNode: areaFocusNode,
+                                      decoration: InputDecoration(
+                                          hintText: "Area/Upozilla/Thana")),
+                                  TextField(
+                                      controller: detailsController,
+                                      focusNode: detailsFocusNode,
+                                      decoration: InputDecoration(
+                                          hintText: "Details Address")),
+                                ],
                               ),
                             ),
-                            Text("Mirpur, Dhaka, Bangladesh")
-                          ],
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text("Cancel")),
+                              FilledButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      district = districtController.text;
+                                      area = areaController.text;
+                                      details = detailsController.text;
+                                    });
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text("Update"))
+                            ],
+                          ));
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Address",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("${details}, ${area}, ${district}")
+                            ],
+                          ),
                         ),
-                      ),
-                      Icon(Icons.arrow_forward_ios)
-                    ],
+                        Icon(Icons.arrow_forward_ios)
+                      ],
+                    ),
                   ),
                 ),
               ),
